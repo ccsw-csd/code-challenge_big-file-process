b7fb43ac51ef3d6ff7c8177c993151746530c74c
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2024-02-08 17:05:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# Code Challenge - Big File Process
+
+Se trata de un 'code challenge' para obtener el algoritmo Java que sea capaz de leer y procesar un fichero de texto enorme en el menor tiempo posible. El fichero tiene más de mil millones de líneas de información y ocupa más de 40 Gb.
+
+
+## Ejecuciones
+
+| #  | Time (min) |      User    |  Description                  |
+|----|------------|--------------|-------------------------------|
+| 1  | 05:12:231  | pajimene     | Línea base |
+
+
+## Requisitos
+
+### Funcionales
+
+El algoritmo deberá leer la información de un fichero de texto plano y procesarla para obtener unos resultados que más adelante comentaremos.
+
+Cada línea (separada por salto de línea) representa una fila de información y dentro de cada fila cada uno de los datos se separa con un ;
+
+Un ejemplo de la estructura de la fila es la siguiente:
+
+```
+Granada;Hombre;Jose;Alzate;Álvarez;9
+Huesca;Hombre;Rocco;Lafuente;Paez;6
+Barcelona;Mujer;Candela;Saez;Luna;6
+Zaragoza;Hombre;Jose;Echeverria;Sánchez;5
+Melilla;Mujer;Gianna;Valentín;Torre;4
+Huelva;Hombre;Rocco;Parra;Natales;7
+València;Mujer;Eluney;Quesada;Molleda;5
+```
+
+- El primer dato es el nombre de la provincia.
+- El segundo dato es el sexo de la persona. Posibles valores -> Mujer / Hombre
+- El tercer dato es el nombre de la persona.
+- El cuarto dato es el primer apellido de la persona.
+- El quinto dato es el segundo apellido de la persona.
+- El sexto dato es el número de personas con ese nombre y apellidos dentro de la provincia. Será un número de una cifra, de 1-9 ambos incluídos.
+
+El algoritmo deberá leer todos esos datos y extraer la siguiente información:
+
+- El valor más alto obtenido comparando todos los nombre de la mujer más repetida en cada provincia.
+- El valor más alto obtenido comparando todos los nombres del hombre más repetido en cada provincia.
+- El valor más bajo obtenido comparando todos los nombres de la mujer menos repetida en cada provincia.
+- El valor más bajo obtenido comparando todos los nombres del nombre del hombre menos repetido en cada provincia.
+
+En el ejemplo de arriba, los resultados deberían ser:
+
+La mujer mas repetida esta en 'Barcelona', se llama 'Candela' y aparece '5' veces
+La mujer menos repetida esta en 'Melilla', se llama 'Gianna' y aparece '4' veces
+El hombre mas repetido esta en 'Granada', se llama 'Jose' y aparece '9' veces
+El hombre menos repetido esta en 'Zaragoza', se llama 'Jose' y aparece '5' veces
+
+
+
+### Técnicos
+
+Todo el código del algoritmo debe estar incluido en un único fichero llamado "BigFileProcessCodeChallenge.java" y se debe respetar el método main del archivo original.
+
+No se podrá utilizar librerías externas que NO formen parte del package de java. Es recomendable utilizar las últimas versiones de Java, pero cada cual puede utilizar la que quiera.
+
+Como ayuda al ejercicio se proveerá un algoritmo "línea base" que funciona correctamente y también estarán accesibles todas aquellas soluciones presentadas por los participantes.
+
+
+## Ejecución
+
+Para obtener un juego de datos válido deberás ejecutar el archivo .jar que acompaña este repositorio git. El comando para ejecutar es
+
+```
+java -jar generateData.jar
+```
+
+Esto generará un fichero "data.txt" de ejemplo, con unas 250 mil líneas y con un tamaño de 100 Mb. Ojo, que necesitas espacio en el disco para esas 100 Mb.
+
+
+Para ejecutar el algoritmo, se acompaña en este repositorio de un proyecto maven que permite la ejecución de la clase principal. Simplemente modifica la clase java del ejemplo de "línea base" o coge como base alguna de las soluciones propuestas y mejora el algoritmo.
+
+A partir de ahí, eres libre de hacer todas las modificaciones que quieras, siempre que cumpla los requisitos funcionales. **Compara siempre tu resultado con la línea base**, así te harás una idea de si es mejor o peor el rendimiento.
+
+
+## Evaluación
+
+Una vez tengas el algoritmo resuelto, envía tu código fuente (la clase implementada) para que pueda ejecutarla en local.
+
+La ejecución se hará siempre en el mismo entorno controlado, un intel i7 de 6 cores y 32 Gb de RAM.
+
+El fichero de prueba será el mismo para todas las ejecuciones y cuenta con un total de 1.066.854.351 líneas y un tamaño de 42 Gb.
+
+
---
+++ src/com/cca/codechallenge/BigFileProcessCodeChallenge.java
@@ -0,0 +1,201 @@
+package com.cca.codechallenge;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BigFileProcessCodeChallenge {
+
+    /** NO MODIFICAR ESTA PARTE **/
+
+    public static void main(String[] args) throws Exception {
+
+        String filename = "./data.txt";
+
+        if (args.length == 1)
+            filename = args[0];
+
+        long start = System.currentTimeMillis();
+        new BigFileProcessCodeChallenge().init(filename);
+        long end = System.currentTimeMillis();
+
+        System.out.println("Duracion: " + (end - start) / 1000.0 + " s");
+    }
+
+    /*****************************/
+
+    public void init(String filename) throws Exception {
+
+        Map<String, ProvinceData> provinceMap = new HashMap<String, ProvinceData>();
+
+        readDataFile(filename, provinceMap);
+        extractStatistics(provinceMap);
+
+    }
+
+    private void readDataFile(String filename, Map<String, ProvinceData> map) throws FileNotFoundException, IOException {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename, StandardCharsets.UTF_8))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String parts[] = line.split(";");
+
+                String provinceName = parts[0];
+                String sex = parts[1];
+                String personName = parts[2];
+                String counter = parts[5];
+
+                ProvinceData provinceData = map.get(provinceName);
+                if (provinceData == null) {
+                    provinceData = new ProvinceData(provinceName);
+                    map.put(provinceName, provinceData);
+                }
+
+                provinceData.addData(sex, personName, counter);
+            }
+        }
+
+    }
+
+    private void extractStatistics(Map<String, ProvinceData> provinceMap) {
+        List<ProvinceTopData> resumeData = new ArrayList<>();
+
+        provinceMap.values().stream().forEach(provinceData -> {
+            resumeData.add(new ProvinceTopData(provinceData));
+        });
+
+        ProvinceTopData orderMostRepeatedWoman = resumeData.stream().sorted(Comparator.comparing(ProvinceTopData::getMostRepeatedWoman, Comparator.comparing(PersonData::getCounter)).reversed()).toList().get(0);
+        System.out.println("La mujer mas repetida esta en '" + orderMostRepeatedWoman.getProvinceName() + "', se llama '" + orderMostRepeatedWoman.getMostRepeatedWoman().getName() + "' y aparece '"
+                + orderMostRepeatedWoman.getMostRepeatedWoman().getCounter() + "' veces");
+
+        ProvinceTopData orderLeastRepeatedWoman = resumeData.stream().sorted(Comparator.comparing(ProvinceTopData::getLeastRepeatedWoman, Comparator.comparing(PersonData::getCounter))).toList().get(0);
+        System.out.println("La mujer menos repetida esta en '" + orderLeastRepeatedWoman.getProvinceName() + "', se llama '" + orderLeastRepeatedWoman.getLeastRepeatedWoman().getName() + "' y aparece '"
+                + orderLeastRepeatedWoman.getLeastRepeatedWoman().getCounter() + "' veces");
+
+        ProvinceTopData orderMostRepeatedMan = resumeData.stream().sorted(Comparator.comparing(ProvinceTopData::getMostRepeatedMan, Comparator.comparing(PersonData::getCounter)).reversed()).toList().get(0);
+        System.out.println("El hombre mas repetido esta en '" + orderMostRepeatedMan.getProvinceName() + "', se llama '" + orderMostRepeatedMan.getMostRepeatedMan().getName() + "' y aparece '"
+                + orderMostRepeatedMan.getMostRepeatedMan().getCounter() + "' veces");
+
+        ProvinceTopData orderLeastRepeatedMan = resumeData.stream().sorted(Comparator.comparing(ProvinceTopData::getLeastRepeatedMan, Comparator.comparing(PersonData::getCounter))).toList().get(0);
+        System.out.println("El hombre menos repetido esta en '" + orderLeastRepeatedMan.getProvinceName() + "', se llama '" + orderLeastRepeatedMan.getLeastRepeatedMan().getName() + "' y aparece '"
+                + orderLeastRepeatedMan.getLeastRepeatedMan().getCounter() + "' veces");
+    }
+
+    class PersonData {
+        private String name;
+        private Long counter;
+
+        public PersonData(String name, Long counter) {
+            this.name = name;
+            this.counter = counter;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Long getCounter() {
+            return counter;
+        }
+
+        public void addCounter(Long counter) {
+            this.counter += counter;
+        }
+
+    }
+
+    class ProvinceData {
+        private String name;
+
+        private Map<String, PersonData> women;
+        private Map<String, PersonData> men;
+
+        public ProvinceData(String name) {
+            this.name = name;
+            this.women = new HashMap<String, PersonData>();
+            this.men = new HashMap<String, PersonData>();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Map<String, PersonData> getMen() {
+            return men;
+        }
+
+        public Map<String, PersonData> getWomen() {
+            return women;
+        }
+
+        public void addData(String sex, String personName, String counter) {
+
+            Map<String, PersonData> map = null;
+
+            if (sex.equals("Hombre"))
+                map = men;
+            else
+                map = women;
+
+            PersonData person = map.get(personName);
+            if (person == null) {
+                person = new PersonData(personName, 0L);
+                map.put(personName, person);
+            }
+
+            person.addCounter(Long.valueOf(counter));
+        }
+    }
+
+    class ProvinceTopData {
+        private String provinceName;
+
+        private PersonData mostRepeatedWoman;
+        private PersonData leastRepeatedWoman;
+
+        private PersonData mostRepeatedMan;
+        private PersonData leastRepeatedMan;
+
+        public ProvinceTopData(ProvinceData data) {
+            this.provinceName = data.getName();
+
+            List<PersonData> womenList = data.getWomen().values().stream().sorted(Comparator.comparing(PersonData::getCounter)).toList();
+            mostRepeatedWoman = womenList.get(womenList.size() - 1);
+            leastRepeatedWoman = womenList.get(0);
+
+            List<PersonData> menList = data.getMen().values().stream().sorted(Comparator.comparing(PersonData::getCounter)).toList();
+            mostRepeatedMan = menList.get(menList.size() - 1);
+            leastRepeatedMan = menList.get(0);
+
+        }
+
+        public String getProvinceName() {
+            return provinceName;
+        }
+
+        public PersonData getMostRepeatedWoman() {
+            return mostRepeatedWoman;
+        }
+
+        public PersonData getLeastRepeatedWoman() {
+            return leastRepeatedWoman;
+        }
+
+        public PersonData getMostRepeatedMan() {
+            return mostRepeatedMan;
+        }
+
+        public PersonData getLeastRepeatedMan() {
+            return leastRepeatedMan;
+        }
+
+    }
+}
